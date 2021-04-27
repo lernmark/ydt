1bc9e9c08398d9f8cb9fb2cf30b6b1a875c1b844
Paul <paul@pamind.se>
2021-04-27 18:25:59 +0200

    Remove psycach

--- ydt/views.py
+++ ydt/views.py
@@ -6,32 +6,61 @@ from rest_framework import permissions, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer, UserSerializerWithToken, TodoSerializer
+import firebase_admin
+from firebase_admin import auth, db, credentials
+from django.core import serializers
+
+from .serializers import UserSerializer, UserSerializerWithToken, TodoSerializer, TokenSerializer
 from .models import Todo
+cred = credentials.Certificate("ydt/serviceAccountKey.json")
+if not firebase_admin._apps:
+    # app = firebase_admin.initialize_app()
+    app = firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://yrkesdorren-bfdfc-default-rtdb.europe-west1.firebasedatabase.app/'
+    })    
 
 @api_view(['GET'])
 def current_user(request):
     """
     Determine the current user by their token, and return their data
-    """    
-    serializer = UserSerializer(request.user)
-    return Response(serializer.data)
+    """   
+    custom_token = auth.create_custom_token(str(request.user)) 
+    ts = TokenSerializer({
+        'firebase_token': custom_token,
+        'username': request.user
+        })
 
+    return Response(ts.data, status=status.HTTP_200_OK)
 
-class UserList(APIView):
-    """
-    Create a new user. It's called 'UserList' because normally we'd have a get
-    method here too, for retrieving a list of all User objects.
-    """
-    permission_classes = (permissions.AllowAny,)
-
-    def post(self, request, format=None):
-        serializer = UserSerializerWithToken(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+def update_firebase(queryset):
+    ref = db.reference('/')
+    ref.delete()
+    objects_to_save = {}
+    for instance in queryset:
+        objects_to_save[instance.id] = {
+            'id': instance.id,
+            'title': instance.title,
+            'description': instance.description,
+            'responsible': instance.responsible,
+            'author': instance.author,
+            'isCompleted': instance.isCompleted,
+            'created_at': str(instance.created_at),
+            'update_at': str(instance.update_at)
+        }
+    ref.set(objects_to_save)
+    
 class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)        
+        update_firebase(Todo.objects.all())
+
+    def perform_update(self, serializer):
+        serializer.save()        
+        update_firebase(Todo.objects.all())
+
+    def perform_destroy(self, serializer):
+        serializer.delete() 
+        update_firebase(Todo.objects.all())
